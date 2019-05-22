cac8f91e926f7db861ed9378152650a7f9880343
cross <ne280107@senshu-u.jp>
2019-05-22 16:06:05 +0900

    外部テキスト読み込みテスト

---
+++ Assets/Scripts/TextReader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using UnityEngine.UI;
+
+public class TextReader : MonoBehaviour
+{
+    private TextAsset textFile;
+    [SerializeField]
+    private Text textAppear;
+
+    private void Start()
+    {
+        textFile = Resources.Load("不思議の国のアリス(一部)") as TextAsset;
+
+        textAppear.text = textFile.text;
+    }
+
+}
