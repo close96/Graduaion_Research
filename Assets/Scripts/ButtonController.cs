9c98a8d27899d8515cf92874f4ea8e9e5d1e8b40
cross <ne280107@senshu-u.jp>
2019-07-03 15:21:33 +0900

    ボタン識別の実装
    ボタンの名前で処理を分岐するテスト

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : BaseButtonController
+{
+    
+    protected override void OnClick(string objectName)
+    {
+        // 渡されたオブジェクト名で処理を分岐
+        if ("YesButton".Equals(objectName))
+        {
+            this.YesButtonClick();
+        } 
+        else if ("NoButton".Equals(objectName))
+        {
+            this.NoButtonClick();
+        }
+        else
+        {
+            throw new System.Exception("Not implemented!!");
+        }
+    }
+
+    private void YesButtonClick()
+    {
+        Debug.Log("はい");
+    }
+
+    private void NoButtonClick()
+    {
+        Debug.Log("いいえ");
+    }
+    
+}
